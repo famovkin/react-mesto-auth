a5e8a1cefdfb8c3b6395373c0574828fda9ca808
Tolya Fam <famovkin@yandex.ru>
2022-01-05 15:22:00 +0300

    fix: добавить атрибут noValidate для форм в попапах

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,6 +6,7 @@ function Form({ handleSubmit, ...props }) {
       onSubmit={handleSubmit}
       className={`popup__form popup__form_type_${props.name}`}
       name={props.name}
+      noValidate
     >
       <h2 className="popup__title">{props.title}</h2>
       {props.children}
